559e0882dc17fe1c8cbb43a3b9b9a4108e41f041
Martin Kowarczyk <martin.kowarczyk@gmail.com>
2023-03-01 16:13:18 +0100

    corrected syntax

--- gutenbergApp/webDB/models.py
+++ gutenbergApp/webDB/models.py
@@ -3,25 +3,25 @@ from django.db import models
 # Create your models here.
 
 class Book (models.Model):
-    id = models.int()
+    ## id = models.int()
     Name = models.CharField(max_length=50)
     publication = models.DateTimeField('date published')
     author = models.CharField(max_length=50)
 
 class Paragraph (models.Model):
-    id = models.int()
-    para_number = models.IntField()
-    parent_document = models.Charfield(Max_length=50)
-    raw_text = models.Charfield(max_length = 10000)
+    ## id = models.int()
+    para_number = models.IntegerField()
+    parent_document = models.CharField(max_length=50)
+    raw_text = models.CharField(max_length = 10000)
 
 class Character (models.Model):
-    id = models.int()
+    ## id = models.int()
     Name = models.CharField(max_length=50)
     Vorname = models.CharField(max_length=50)
     Nachname = models.CharField(max_length=50)
-    parent_document = models.Charfield(Max_length=50)
+    parent_document = models.CharField(max_length=50)
 
-class Topic (models.model):
-    id = models.int()
+class Topic (models.Model):
+    ## id = models.int()
     topic_name = models.CharField(max_length=50)
     keywords = models.CharField(max_length = 50)
