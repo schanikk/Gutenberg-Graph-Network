e906b283c791f8a9e38e1ccab81c844405aaa508
Sanaz Dehghani <sanaz.khanpour@yahoo.de>
2023-03-01 15:17:07 +0100

    Updated Views

--- gutenbergApp/webDB/views.py
+++ gutenbergApp/webDB/views.py
@@ -1,14 +1,32 @@
-from django.shortcuts import render
-from rest_framework import generics
-from .models import Snippet
-from .serializers import SnippetSerializer
+from django.contrib.auth.models import Book, Paragraph, Charakter, Topic
+from rest_framework import viewsets
+from rest_framework import permissions
+from gutenbergApp.webDB.Serializers import BookSerializer, CharakterSerializer, ParagraphSerializer, TopicSerializer
+from tutorial.quickstart.serializers import UserSerializer, GroupSerializer
 
 
-class SnippetList(generics.ListCreateAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class BookViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Book.objects.all().order_by('-date_joined')
+    serializer_class = BookSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
 
-class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+class ParagraphViewSet(viewsets.ModelViewSet):
+    queryset = Paragraph.objects.all()
+    serializer_class = ParagraphSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class CharakterSet(viewsets.ModelViewSet):
+    queryset = Charakter.objects.all().order_by('-date_joined')
+    serializer_class = CharakterSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+    permission_classes = [permissions.IsAuthenticated]
