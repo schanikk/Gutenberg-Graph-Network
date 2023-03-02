fd7f81b32d0499413a29939bd514b17db31d6ae4
Till Schaland <till.nikolaus.schaland@web.de>
2023-03-02 15:57:32 +0100

    implemented basic restapi responses for testing

--- gutenbergApp/webDB/views.py
+++ gutenbergApp/webDB/views.py
@@ -1,33 +1,40 @@
-from .models import Book, Paragraph, Character, Topic
-from rest_framework import viewsets
-from rest_framework import permissions
-from .Serializers import BookSerializer, CharacterSerializer, ParagraphSerializer, TopicSerializer
+from django.http import JsonResponse
+
+def api_home(request,*args, **kwargs):
+    return JsonResponse({"message":"Hello From the API!!!"})
 
 
-
-class BookViewSet(viewsets.ModelViewSet):
-    """
-    API endpoint that allows users to be viewed or edited.
-    """
-    queryset = Book.objects.all() # .order_by('-date_joined')
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-
-class ParagraphViewSet(viewsets.ModelViewSet):
-    queryset = Paragraph.objects.all()
-    serializer_class = ParagraphSerializer
-    permission_classes = [permissions.IsAuthenticated]
+def collection(request,*args, **kwargs):
+    response = {
+            "data": [
+                {"id":1, "name":"Book1", "author":"Author1"},
+                {"id":2, "name":"Book2", "author":"Author2"},
+                {"id":3, "name":"Book3", "author":"Author3"},
+                {"id":4, "name":"Book4", "author":"Author4"},
+                ]
+            }
+    return JsonResponse(response)
 
 
-class CharacterViewSet(viewsets.ModelViewSet):
-    queryset = Character.objects.all()
-    serializer_class = CharacterSerializer
-    permission_classes = [permissions.IsAuthenticated]
+def book(request,*args, **kwargs):
+    response = {
+            "data": [
+                {"id":1, "name":"Character1", "parent":1},
+                {"id":2, "name":"Character2", "parent":1},
+                {"id":3, "name":"Character3", "parent":1},
+                {"id":4, "name":"Character4", "parent":1},
+                ]
+            }
+    return JsonResponse(response)
 
 
-class TopicViewSet(viewsets.ModelViewSet):
-    queryset = Topic.objects.all()
-    serializer_class = TopicSerializer
-    permission_classes = [permissions.IsAuthenticated]
-    
+def character(request,*args, **kwargs):
+    response = {
+            "data": [
+                {"id":1, "name":"Topic1", "couint":25},
+                {"id":2, "name":"Topic2", "count":5},
+                {"id":3, "name":"Topic3", "count":55},
+                {"id":4, "name":"Topic4", "count":12},
+                ]
+            }
+    return JsonResponse(response)
