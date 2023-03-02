63f1a9537e3f41b1d4bca506ee5c28b7ed415012
Till Schaland <till.nikolaus.schaland@web.de>
2023-03-02 12:41:06 +0100

    added datapipeline

--- gutenbergCorpus/main.py
+++ gutenbergCorpus/main.py
@@ -1,10 +1,90 @@
-import pandas as pd
-from .src.topicmodelling import ( topicmodell, SDTopicmodel )
-from .src.namedentity import (  )
+# Internal
+import os
 
+#External
+import pandas as pd
+
+#Relative
+from src.topicmodelling.topicmodell import ( TopicModel )
+from src.namedentity.namedentities import ( NamedEntity )
+from src.util.data_loader import  ( loadMeta, DataLoader, preprocess )
+from definitions import TEXT_DIR
+
+
+def getBookNames(filenames:list) -> list:
+    file_names = list()
+    for file in filenames:
+        file_names.append(file.split('_')[0])
+    
+    print(file_names)
+    return file_names
+
+def filterAndCombine(df, filenames):
+    
+    #GetIndexOfDataFrame Entry for each book
+    booksDfIndex = getBookNames(filenames)
+    print(booksDfIndex) 
+    
+    df = df.set_index('id')
+    filteredDF = df.filter(items=booksDfIndex, axis=0)
+
+    # Create New Series with FullText for new Column
+    text_series = dict()
+    for i,_ in filteredDF.iterrows():
+        print(i)
+        text_series[i] = DataLoader(i)
+    
+    #Insert Text
+    filteredDF['text'] = text_series
+
+    filteredDF['paras'] = filteredDF.text.apply(preprocess)
+
+    filteredDF = filteredDF.reset_index()
+
+    filteredDF = filteredDF.rename({'index': 'BookFileName'})
+
+    return filteredDF
+
+
+
+def main():
+    # Initialize Models
+ #   tm_model = TopicModel()
+ #   ner_model = NamedEntity()
+
+    # Get all Meta Data
+    df_meta = loadMeta()
+
+    # Get all Files in text Directory
+    listOfBooks = os.listdir(TEXT_DIR)
+
+    #Filter Metadata  to only containing Needed Data
+    # For each file load the text  into  the  dataframe
+    df_filtered = filterAndCombine(df_meta, listOfBooks)
+    
+    #DEBBUG
+    print(df_filtered[:10])
+
+    
+    #Only 100 books
+    df_filtered = df_filtered[:1000]
+
+
+    df_filtered.to_json("./data/checkpoint/temporarydocs.json", orient='records')
+
+    # Train TopicModell
+
+
+
+    # retrieve document_info
+
+    # for each document -> NER
+
+
+    # save to json
 
 
 
 
 if __name__ == "__main__":
-    pass
+    main()
--- gutenbergCorpus/src/namedentity/namedentities.py
+++ gutenbergCorpus/src/namedentity/namedentities.py
@@ -1,34 +1,22 @@
 import spacy
-import nltk
-from nltk.corpus import gutenberg
+import spacy_transformers
 
-def setup():
-    nlp = spacy.load("en_core_web_trf")
-    return nlp
+class NamedEntity():
+    def __init__(self) -> None:
+        self.nlp = spacy.load("en_core_web_trf")
+        self.filter_words = ["he", "she", "was:--he", "himself", "herself", "himself;--"] 
 
-def create_entities(docs):
-    pronoun = ["he", "she", "was:--he", "himself", "herself", "himself;--"]
-    dict_ = dict()
-    for key,value in docs.items():
-        print(f"Para {key} ")
-        for span in value.ents:
-            temp_list = list()
-            if span.label_ == "PERSON" and span.text not in pronoun:
-                temp_list.append((span.text, span.label_, span.start, span.end))
-                print(span.text, span.start, span.end, span.label_)
-        dict_[key] = temp_list
-    return dict_
+    def process(self, doc):
+        return self.nlp(doc)
 
-def load_and_process(nlp):
-    fileids_ = gutenberg.fileids()
-    final_result = dict()
-    for fileid in fileids_:
-        doc = gutenberg.paras(fileid)
-        doc = [' '.join(e) for para in doc for e in para]
-        results_ = dict()
-        for i, d in enumerate(doc):
-            res = nlp(d)
-            results_[i] = res
-        final_result[fileid]=create_entities(results_)
+    def extractPersons(self, doc):
+        temp_list = list()
+        for ent in doc.ents:
+            if ent.label_ == "PERSON" and ent.text not in self.filter_words:
+                temp_list.append({'text': ent.text, 'label':ent.label_, 'start_char': ent.start, 'end_char': ent.end})
 
-    return final_result
+        return temp_list
+
+    def processAndExtract(self, doc):
+        doc = self.process(doc)
+        return self.extractPersons(doc)
