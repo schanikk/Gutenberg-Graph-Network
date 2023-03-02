8277178f34b6c98a7291f3576a3ecc4ee5ddff92
Martin Kowarczyk <martin.kowarczyk@gmail.com>
2023-03-02 16:14:04 +0100

    Fixed routing

--- gutenbergApp/webDB/urls.py
+++ gutenbergApp/webDB/urls.py
@@ -7,8 +7,8 @@ router = routers.DefaultRouter()
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.api_home)
-    path('collection', views.collection)
-    path('book', views.book)
-    path('character', views.character)
+    path('', views.api_home),
+    path('collection/', views.collection),
+    path('book/', views.book),
+    path('character/', views.character)
 ]
