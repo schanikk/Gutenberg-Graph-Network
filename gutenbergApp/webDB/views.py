0ec05010e5ea91500c5cbac3d8d26fc2714b85bb
Sanaz Dehghani <sanaz.khanpour@yahoo.de>
2023-03-03 11:33:52 +0100

    uploading books

--- gutenbergApp/webDB/views.py
+++ gutenbergApp/webDB/views.py
@@ -1,4 +1,7 @@
 from django.http import JsonResponse
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from .forms import ModelFormWithFileField
 
 def api_home(request,*args, **kwargs):
     return JsonResponse({"message":"Hello From the API!!!"})
@@ -38,3 +41,20 @@ def character(request,*args, **kwargs):
                 ]
             }
     return JsonResponse(response)
+
+
+def upload_file(request):
+    if request.method == 'POST':
+        form = ModelFormWithFileField(request.POST, request.FILES)
+        if form.is_valid():
+            # file is saved
+            form.save()
+            return HttpResponseRedirect('/success/url/')
+    else:
+        form = ModelFormWithFileField()
+    return render(request, 'upload.html', {'form': form})
+
+def handle_uploaded_file(f):
+    with open('some/file/name.txt', 'wb+') as destination:
+        for chunk in f.chunks():
+            destination.write(chunk)
