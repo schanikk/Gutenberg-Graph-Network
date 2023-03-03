74e3ec0165c728115d256188e74ece10a9423a9d
Till Schaland <till.nikolaus.schaland@web.de>
2023-03-03 01:18:46 +0100

    Docker-Compose Running, psql up
    - implemented docker containerization with docker-compose
    - fixxed somebugs which caused docker to not run

--- gutenbergApp/webDB/urls.py
+++ gutenbergApp/webDB/urls.py
@@ -6,8 +6,8 @@ from webDB import views
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', views.api_home)
-    path('collection', views.collection)
-    path('book', views.book)
+    path('', views.api_home),
+    path('collection', views.collection),
+    path('book', views.book),
     path('character', views.character)
 ]
