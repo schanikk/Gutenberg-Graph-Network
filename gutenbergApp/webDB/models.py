5d5ccdc39ac8f807ee172e0f6f1029df8c2a39ee
Till Schaland <till.nikolaus.schaland@web.de>
2023-03-03 13:40:15 +0100

    added models

--- gutenbergApp/webDB/models.py
+++ gutenbergApp/webDB/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
+
 # Create your models here.
+class Book(models.Model):
+    bookID = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=100, blank=True, default='')
+    author = models.TextField()
+    class Meta:
+        ordering = ['created']
+
+
+
+class Character(models.Model):
+    characterID = models.CharField(max_length=100,blank=True, default='')
+    name = models.CharField(max_length=100, blank=True, default='')
+    bookID = models.TextField(max_length=100,blank=True,default='')
+    class Meta:
+        ordering = ['created']
+
+class Sentence(models.Model):
+    sentenceText = models.CharField(max_length=1000,blank=True, default='')
+    bookID = models.CharField(max_length=100,blank=True,default='')
+
+
+class Topic(models.Model):
+    topicID = models.CharField(max_length=100, blank=True, default='')
+    topicName = models.CharField(max_length=100, blank=True, default='')
+    class Meta:
+        ordering = ['created']
+
+
+
+class Character2Sentence(models.Model):
+    characterID = models.CharField(mx_length=100, blank=True, default='')
+    sentenceID = models.CharField(max_length=100, blank=True, default='')
+    class Meta:
+        ordering = ['created']
+
+
+
