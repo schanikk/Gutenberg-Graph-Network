e10878a6850e320a70e7c6f61dab9a65587dfc18
Till Schaland <till.nikolaus.schaland@web.de>
2023-03-04 16:18:07 +0100

    adjusted Models

--- gutenbergApp/webDB/models.py
+++ gutenbergApp/webDB/models.py
@@ -1,22 +1,31 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
 # Create your models here.
 class Book(models.Model):
     bookID =  models.CharField(max_length=100, blank=True, default='')
-    bookName = models.CharField(max_length=100, blank=True, default='')
-    bookauthor = models.TextField()
+    bookName = models.CharField(max_length=200, blank=True, default='')
+    bookauthor = models.CharField(maxx_length=200, blank=True, default='')
+    bookAuthorYearOfBirth=models.CharField(max_length=5, blank=True,default='')
+    bookAuthorYearOfDeath=models.CharField(max_length=5, blank=True,default='')
+    bookLanguage=models.CharField(max_length=100, blank=True,default='')
+    bookSubjects=models.CharField(max_length=200, blank=True,default='')
     # class Meta:
     #     ordering = ['created']
 
 class Topic(models.Model):
     topicID = models.CharField(max_length=100, blank=True, default='')
     topicName = models.CharField(max_length=100, blank=True, default='')
+    Top_n_words = ArrayField(
+            models.CharField(max_length=20, blank=True),
+            size=15,
+        )
+    bookIndex = models.ForeignKey(Book, on_delete=models.CASCADE)
     # class Meta:
     #     ordering = ['created']
 
 
 class Character(models.Model):
-    characterID = models.CharField(max_length=100,blank=True, default='')
     name = models.CharField(max_length=100, blank=True, default='')
     ## bookID = models.TextField(max_length=100,blank=True,default='')
     bookID = models.ForeignKey(Book, on_delete=models.CASCADE)
@@ -24,20 +33,12 @@ class Character(models.Model):
     #     ordering = ['created']
 
 class Sentence(models.Model):
-    sentenceText = models.CharField(max_length=1000,blank=True, default='')
-    ## bookID = models.CharField(max_length=100,blank=True,default='')
     bookID = models.ForeignKey(Book, on_delete=models.CASCADE)
-    sentenceIndex = models.IntegerField(blank=True, default=0)
-    # topicID = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    sentenceText = models.CharField(max_length=1000,blank=True, default='')
+    sentenceIDBook=models.IntegerField()
+    topicID=models.ForeignKey(Book,on_delete=models.CASCADE)
 
 
-# class Character2Sentence(models.Model):
-#     ## characterID = models.CharField(mx_length=100, blank=True, default='')#
-#     characterID = models.ForeignKey(Character, on_delete=models.CASCADE)
-#     ## sentenceID = models.CharField(max_length=100, blank=True, default='')
-#     sentenceID = models.ForeignKey(Sentence, on_delete=models.CASCADE)
-#     # class Meta:
-#     #     ordering = ['created']
-
-
-
+class sent2char(models.Model):
+    sentID = models.ForeignKey(Sentence, on_delete=models.CASCADE)
+    charID = models.ForeignKey(Character, on_delete=models.CASCADE)
