3a659fab3294eceeabf9556d9df0dcbd60898f0a
Martin Kowarczyk <martin.kowarczyk@gmail.com>
2023-03-06 10:58:09 +0100

    Js Logic adjusted, corrected compose yml

--- gutenbergApp/static/scripts/index.js
+++ gutenbergApp/static/scripts/index.js
@@ -13,20 +13,33 @@ let topics = [];
 const resetBtn = document.querySelector(".reset-btn");
 const COLL_API = "http://localhost:8000/api/collection/";
 const BOOK_API = "http://localhost:8000/api/book/";
-// const CHAR_API = "http://localhost:8000/api/character/";
+const CHAR_API = "http://localhost:8000/api/character/";
 
 // TODO: Anpassung der Listeneinträge für jede Column
-function createListEntry(item, ...column) {
+function createBookEntry(item, ...column) {
   if (column[0] == "topic") {
-    return `<li class="list-group-item d-flex justify-content-between align-items-center" data-dbid='${item.bookID}'>${item.bookName}<span class="badge text-bg-warning rounded-pill">14</span></li>`;
+    return `<li class="list-group-item d-flex justify-content-between align-items-center" data-dbid='${
+      item.pk
+    }'>${item.fields.bookName.toUpperCase()}<span class="badge text-bg-warning rounded-pill">14</span></li>`;
   }
-  return `<li class="list-group-item d-flex justify-content-between align-items-center" data-dbid='${item.bookID}'>${item.bookName}</li>`;
+  return `<li class="list-group-item d-flex justify-content-between align-items-center" data-dbid='${
+    item.pk
+  }'>${item.fields.bookName.toUpperCase()}</li>`;
+}
+function createTopicEntry(item, ...column) {
+  return `<li class="list-group-item d-flex justify-content-between align-items-center" data-dbid=''>${item.TopicName}<span class="badge text-bg-warning rounded-pill">${item.Count}</span></li>`;
+}
+
+function createPersonEntry(item, ...column) {
+  return `<li class="list-group-item d-flex justify-content-between align-items-center" data-dbid='${
+    item.pk
+  }'>${item.fields.Name.toUpperCase()}</li>`;
 }
 
 function fillUpColumns(books, persons, topics) {
   books.forEach((item) => {
     console.log(item.fields);
-    bookList.innerHTML += createListEntry(item.fields);
+    bookList.innerHTML += createBookEntry(item);
   });
   //persons.forEach((item) => {
   //  personList.innerHTML += createListEntry(item);
@@ -45,8 +58,7 @@ async function getData(url) {
 getAllData().then(() => {
   fillUpColumns(books, persons, topics);
   bookItems = bookList.querySelectorAll("ul > li");
-  // personItems = personList.querySelectorAll("ul > li");
-  // topicItems = topicList.querySelectorAll("ul > li");
+  topicItems = topicList.querySelectorAll("ul > li");
   addAllListeners();
 });
 
@@ -55,18 +67,6 @@ async function getAllData() {
     books = JSON.parse(data);
     console.log(books);
   });
-  // .then(
-  //   await getData(BOOK_API).then((data) => {
-  //     persons = data.data;
-  //     console.log(persons);
-  //   })
-  // )
-  // .then(
-  //   await getData(CHAR_API).then((data) => {
-  //     topics = data.data;
-  //     console.log(topics);
-  //   })
-  // );
 }
 
 // function sleep(ms) {
@@ -76,7 +76,7 @@ async function getAllData() {
 let selectedBook = "";
 let selectedPerson = "";
 
-function addListeners(listeningItems, affectedItems, selectedListener) {
+function addBookListeners(listeningItems, affectedItems, selectedListener) {
   for (let item of listeningItems) {
     item.addEventListener("click", async (e) => {
       console.clear();
@@ -86,12 +86,46 @@ function addListeners(listeningItems, affectedItems, selectedListener) {
         listeningItem.style.opacity = "0.5";
       }
 
-      // if (selectedListener !== e.target.innerText.match(/^[a-zA-Z]+\d{1}/)[0]) {
-      //   selectedListener = e.target.innerText.match(/^[a-zA-Z]+\d{1}/)[0];
-      //   e.target.style.opacity = "1";
-      // } else {
-      //   e.target.style.opacity = "1";
-      // }
+      if (selectedListener !== id) {
+        selectedListener = id;
+        e.target.style.opacity = "1";
+      } else {
+        e.target.style.opacity = "1";
+      }
+
+      persons = await getData(BOOK_API + id).then((data) => {
+        json = JSON.parse(data);
+        return json;
+      });
+      console.log(persons);
+
+      if (personList.innerHTML !== "") {
+        personList.innerHTML = "";
+      }
+
+      persons.map((item) => {
+        if (item.fields["Name"].match(/.*-+.*/)) {
+          console.log(item);
+        }
+      });
+      persons.forEach((item) => {
+        personList.innerHTML += createPersonEntry(item);
+      });
+      personItems = personList.querySelectorAll("ul > li");
+      addPersonListeners(personItems, topicItems, selectedPerson);
+    });
+  }
+}
+
+function addPersonListeners(listeningItems, affectedItems, selectedListener) {
+  for (let item of listeningItems) {
+    item.addEventListener("click", async (e) => {
+      console.clear();
+      let id = e.target.dataset.dbid;
+      console.log(id);
+      for (let listeningItem of listeningItems) {
+        listeningItem.style.opacity = "0.5";
+      }
 
       if (selectedListener !== id) {
         selectedListener = id;
@@ -100,44 +134,54 @@ function addListeners(listeningItems, affectedItems, selectedListener) {
         e.target.style.opacity = "1";
       }
 
-      // for (let affectedItem of affectedItems) {
-      //   console.log(affectedItem);
-      //   console.log(
-      //     "affected: ",
-      //     affectedItem.innerText.match(/^[a-zA-Z]+\d{1}/)[0]
-      //   );
-      //   console.log("selected: ", selectedListener);
-
-      //   if (
-      //     affectedItem.innerText.match(/^[a-zA-Z]+\d{1}/)[0] !==
-      //     selectedListener
-      //   ) {
-      //     affectedItem.classList.remove("d-flex");
-      //     affectedItem.classList.add("d-none");
-      //   } else {
-      //     affectedItem.classList.add("d-flex");
-      //     affectedItem.classList.remove("d-none");
-      //     affectedItem.style.opacity = "1";
-      //   }
-      // }
-      persons = await getData(BOOK_API + id).then((data) => {
+      topicsJSON = await getData(CHAR_API + id).then((data) => {
         json = JSON.parse(data);
+        console.log();
         return json;
       });
+      console.log(topicsJSON);
+      topics = [];
 
-      if (personList.innerHTML !== "") {
-        personList.innerHTML = "";
+      for (let i = 0; i < 500; i++) {
+        if (topicsJSON[i] == undefined) break;
+        console.log(topicsJSON[i]);
+        console.log(topicsJSON[i]["TopicName:"].split(/_/g));
+
+        tempTn = topicsJSON[i]["TopicName:"].split(/_/g);
+        console.log(tempTn);
+        tempTn.shift();
+
+        let tn = tempTn.join(" ").toUpperCase();
+
+        topicsJSON[i]["TopicName:"] = tn;
+
+        topics.push({
+          TopicName: topicsJSON[i]["TopicName:"],
+          Count: topicsJSON[i]["Count"],
+        });
       }
-      persons.forEach((item) => {
-        personList.innerHTML += createListEntry(item);
+
+      console.log(topics);
+
+      if (topicList.innerHTML !== "") {
+        topicList.innerHTML = "";
+      }
+
+      // persons.map((item) => {
+      //   if (item.fields["Name"].match(/.*-+.*/)) {
+      //     console.log(item);
+      //   }
+      // });
+      topics.forEach((item) => {
+        topicList.innerHTML += createTopicEntry(item);
       });
     });
   }
 }
 
 function addAllListeners() {
-  addListeners(bookItems, personItems, selectedBook);
-  //addListeners(personItems, topicItems, selectedPerson);
+  addBookListeners(bookItems, personItems, selectedBook);
+
   resetBtn.addEventListener("click", (e) => {
     for (let bookItem of bookItems) {
       bookItem.classList.remove("d-none");
